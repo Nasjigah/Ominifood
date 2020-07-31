8869bb23660f5eb24aba9aa35ece22055576bc55
Abdul Nasser Bagigah <65161807+Nasjigah@users.noreply.github.com>
2020-08-01 00:28:45 +0300

    Restore Commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
     
-     
-    /** Sticky navigation *////
+    /* STICKY NAVIGATION */
     
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-             $('nav').addClass('sticky');
-        } else {
+   $('.js--section-home').waypoint(function(direction) {
+       if (direction == "down") {
+           $('nav').addClass('sticky');
+       } else {
            $('nav').removeClass('sticky');
-        }
-        
-    }, {
-  offset: '60px;'
-});
+       }
+   }, {
+      offset: '25%'
+    });
     
+    /* SCROLLING TO ELEMENTS */
     
-    /** Scroll n buttons *////
+    $('.js--scroll-signup').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 1000);
+    });
     
-    $('.js--scroll-to-plans').click(function() {
-        $('html, body').animate({scrollTop:$('.js--section-plans').offset().top}, 1000);
+    $('.js--scroll-preparation').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-home').offset().top}, 1000);
     });
     
     
-    $('.js--scroll-to-start').click(function() {
-        $('html, body').animate({scrollTop:$('.js--section-features').offset().top}, 1000);
-    });
-   
-    /** Scroll nav **/
+    /* SMOOTH SCROLL */
+    
     
     $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -64,59 +62,53 @@ $(document).ready(function() {
     }
   });
     
-    /* Animation scroll */
+    /* ANIMATION ON SCROLL */
     
-    $('.js--wp-1').waypoint(function(direction){
-        $('.js--wp-1').addClass('animated animate__fadeIn');
-        
-    }, {
-  offset: '50%'
-    });
     
-     $('.js--wp-2').waypoint(function(direction){
-        $('.js--wp-2').addClass('animated animate__fadeInUp');
-        
+    $('.js--wp-1').waypoint(function(direction) {
+       $('.js--wp-1').addClass('animated animate__fadeInRight');
     }, {
-  offset: '50%'
-    });
-    
-     $('.js--wp-3').waypoint(function(direction){
-        $('.js--wp-3').addClass('animated animate__fadeIn');
-        
-    }, {
-  offset: '50%'
-    });
-    
-     $('.js--wp-4').waypoint(function(direction){
-        $('.js--wp-4').addClass('animated animate__pulse');
-        
-    }, {
-  offset: '50%'
+      offset: '50%'
     });
     
     
     
-    /* Mobile nav */
+    $('.js--wp-2').waypoint(function(direction) {
+       $('.js--wp-2').addClass('animated animate__pulse');
+    }, {
+      offset: '25%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+       $('.js--wp-3').addClass('animated animate__fadeInUpBig');
+    }, {
+      offset: '90%'
+    });
+    
+    
+    $('.js--wp-4').waypoint(function(direction) {
+       $('.js--wp-4').addClass('animated animate__pulse');
+    }, {
+      offset: '50%'
+    });
+    
+    
+     /* MOBILE NAV */
     
     $('.js--nav-icon').click(function() {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
         
         nav.slideToggle(200);
-        if(icon.hasClass('ion-navicon-round')) {
+        if (icon.hasClass('ion-navicon-round')) {
             icon.addClass('ion-close-round');
             icon.removeClass('ion-navicon-round');
         } else {
             icon.addClass('ion-navicon-round');
             icon.removeClass('ion-close-round');
         }
+        
+
     });
     
 });
-
-
-
-
-
-
-
